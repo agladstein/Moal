e47d864323a7696441dea57867b7f1b6316ff5e9
Ariella Gladstein <agladstein@vm142-41.cyverse.org>
2018-08-13 12:29:50 -0700

    Add file with functions and create function to convert minutes to decimal format

---
+++ functions.py
@@ -0,0 +1,13 @@
+def convert_mins_to_decimal(time):
+    """
+    Convert minutes in time to decimal notation.
+    
+    Args: time (str): time given in hrs:mins
+    Returns: decimal (float): time given in hrs.mins/60*100
+    """
+    time_split = time.split(':')
+    hrs = time_split[0]
+    mins = int(time_split[1])
+    percent = int((mins/60)*100)
+    decimal = float('{}.{}'.format(str(hrs),str(percent)))
+    return(decimal)
